ba67bd8f86d5463ad191f6806eb0d4945e3a1a80
Simon Jäger <simon-jaeger@users.noreply.github.com>
2020-03-02 16:39:35 +0100

    modal

---
+++ resources/spa/components/ModalPlaylists.vue
@@ -0,0 +1,36 @@
+<!-- TODO: wip, add functionality -->
+<template>
+  <Modal title="Add to..." id="modalPlaylists">
+    <div class="checks">
+      <div class="check">
+        <input type="checkbox" checked id="1" name="fantasy-and-rpgs">
+        <label for="1" class="check_label">Fantasy and RPGs</label>
+      </div>
+      <div class="check">
+        <input type="checkbox" id="2" name="fantasy-and-rpgs">
+        <label for="2" class="check_label">Weekend Playlist</label>
+      </div>
+      <div class="check">
+        <input type="checkbox" id="3" name="fantasy-and-rpgs">
+        <label for="3" class="check_label">Relaxing Tracks</label>
+      </div>
+    </div>
+
+    <template v-slot:actions>
+      <button class="_">+ New playlist</button>
+      <button class="_">Done</button>
+    </template>
+  </Modal>
+</template>
+
+<script>
+  import Modal from "./Modal"
+  export default {
+    name: "ModalPlaylists",
+    components: {Modal}
+  }
+</script>
+
+<style scoped>
+
+</style>
